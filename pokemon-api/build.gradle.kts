253b2339add1ee3ff101e85b9d4b7f8bba27ebd5
PolinaPolupan <polupanpolina9@gmail.com>
2025-01-18 17:27:03 +0300

    feat: Setup batching tables

--- pokemon-api/build.gradle.kts
+++ pokemon-api/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.security)
+    implementation(libs.spring.boot.starter.batch)
     implementation(libs.spring.boot.starter.oauth2.resource.server)
     implementation(libs.flyway.core)
     implementation(libs.flyway.database.postgresql)
