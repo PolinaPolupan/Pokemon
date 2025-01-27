a26943a8b80eab70c542087c3f52fb451655cf4f
PolinaPolupan <polupanpolina9@gmail.com>
2025-01-27 16:13:33 +0300

    refactor: Remove unused auth module. Setup container

--- pokemon-api/src/main/java/example/pokemon/security/RestConfig.java
+++ pokemon-api/src/main/java/example/pokemon/security/RestConfig.java
@@ -1,32 +1,18 @@
 package example.pokemon.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
-import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.security.interfaces.RSAPublicKey;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.Collection;
 import java.util.List;
@@ -34,80 +20,11 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class RestConfig {
-
-//    @Bean
-//    JwtAuthenticationConverter jwtAuthenticationConverter() {
-//        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
-//
-//            String role = jwt.getClaimAsString("role");
-//
-//            if (role != null) {
-//                SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role);
-//                return Collections.singletonList(authority);
-//            }
-//
-//            return Collections.emptyList();
-//        });
-//
-//        return jwtAuthenticationConverter;
-//    }
-//
-//    @Value("${jwt.public.key}")
-//    RSAPublicKey key;
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers(HttpMethod.POST, "/api/v1/cards/**")
-//                        .hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.POST, "/api/v1/students/**")
-//                        .hasRole("ADMIN")
-//                        .requestMatchers("/actuator/**").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//            .httpBasic(Customizer.withDefaults())
-//            .oauth2ResourceServer((oauth2) -> oauth2
-//                        .jwt(jwt -> jwt
-//                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
-//                        )
-//                )
-//            .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//            .exceptionHandling((exceptions) -> exceptions
-//                    .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-//                    .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
-//            );
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    JwtDecoder jwtDecoder() {
-//        return NimbusJwtDecoder.withPublicKey(this.key).build();
-//    }
-
     interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {}
 
     @Bean
@@ -147,7 +64,7 @@ public class RestConfig {
 
         http.authorizeHttpRequests(requests -> {
             requests.requestMatchers("/me").authenticated();
-            requests.anyRequest().denyAll();
+            requests.anyRequest().authenticated();
         });
 
         return http.build();
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,4 @@
 rootProject.name = "pokemon"
 include("pokemon-api")
-include("pokemon-auth")
 include("pokemon-admin-server")
 include("pokemon-client")
