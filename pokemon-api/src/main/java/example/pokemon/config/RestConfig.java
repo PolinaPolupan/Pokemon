a2239401f21541a9ef3377e7f94d1f2efb335ea2
PolinaPolupan <polupanpolina9@gmail.com>
2025-01-28 14:45:48 +0300

    refactor: Rename package

--- pokemon-api/src/main/java/example/pokemon/config/RestConfig.java
+++ pokemon-api/src/main/java/example/pokemon/config/RestConfig.java
@@ -1,4 +1,4 @@
-package example.pokemon.security;
+package example.pokemon.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
