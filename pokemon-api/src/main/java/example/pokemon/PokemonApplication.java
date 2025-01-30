51bf901e1870720c29df457c68b8ba4384f9f4a4
PolinaPolupan <polupanpolina9@gmail.com>
2025-01-30 19:59:25 +0300

    feat: Add caching to the tcg client response

--- pokemon-api/src/main/java/example/pokemon/PokemonApplication.java
+++ pokemon-api/src/main/java/example/pokemon/PokemonApplication.java
@@ -2,13 +2,10 @@ package example.pokemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PokemonApplication {
 
 	public static void main(String[] args) {
--- pokemon-api/src/main/java/example/pokemon/dto/CardImageResponse.java
+++ pokemon-api/src/main/java/example/pokemon/dto/CardImageResponse.java
@@ -3,11 +3,12 @@ package example.pokemon.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CardImageResponse {
+public class CardImageResponse implements Serializable {
     private String name;
     private String imageUrl;
 }
