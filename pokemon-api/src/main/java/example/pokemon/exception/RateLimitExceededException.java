29029144a870c6afb82c3d74e86eceee3c6fd3b1
PolinaPolupan <polupanpolina9@gmail.com>
2025-02-02 20:15:08 +0300

    feat: Add Swagger Ui

--- pokemon-api/src/main/java/example/pokemon/exception/RateLimitExceededException.java
+++ pokemon-api/src/main/java/example/pokemon/exception/RateLimitExceededException.java
@@ -1,9 +1,5 @@
 package example.pokemon.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.TOO_MANY_REQUESTS)
 public class RateLimitExceededException extends RuntimeException {
     public RateLimitExceededException(String message) {
         super(message);
