2c495641e20ee955565e652cdb0df348a0294c9e
PolinaPolupan <polupanpolina9@gmail.com>
2025-02-03 19:42:50 +0300

    feat: Setup OAuth2 with Swagger

---
+++ pokemon-api/src/main/java/example/pokemon/config/OpenAPISecurityConfig.java
@@ -0,0 +1,51 @@
+package example.pokemon.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenAPISecurityConfig {
+
+    @Value("${keycloak.auth-server-url}")
+    String authServerUrl;
+    @Value("${keycloak.realm}")
+    String realm;
+
+    private static final String OAUTH_SCHEME_NAME = "Pokemon_OAuth2";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().components(new Components()
+                        .addSecuritySchemes(OAUTH_SCHEME_NAME, createOAuthScheme()))
+                .addSecurityItem(new SecurityRequirement().addList(OAUTH_SCHEME_NAME))
+                .info(new Info().title("Pokemon")
+                        .version("1.0"));
+    }
+
+    private SecurityScheme createOAuthScheme() {
+        OAuthFlows flows = createOAuthFlows();
+        return new SecurityScheme().type(SecurityScheme.Type.OAUTH2)
+                .flows(flows);
+    }
+
+    private OAuthFlows createOAuthFlows() {
+        OAuthFlow flow = createAuthorizationCodeFlow();
+        return new OAuthFlows().authorizationCode(flow);
+    }
+
+    private OAuthFlow createAuthorizationCodeFlow() {
+        return new OAuthFlow()
+                .tokenUrl(authServerUrl + "/realms/" + realm + "/protocol/openid-connect/token")
+                .authorizationUrl(authServerUrl + "/realms/" + realm + "/protocol/openid-connect/auth");
+    }
+}
