0e9a81f1bc1a7aad6bc5d08524ce525128b5d8db
PolinaPolupan <polupanpolina9@gmail.com>
2025-02-05 19:52:09 +0300

    feat: Setup documents

--- pokemon-api/src/main/java/example/pokemon/model/Card.java
+++ pokemon-api/src/main/java/example/pokemon/model/Card.java
@@ -11,11 +11,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.util.List;
 import java.util.UUID;
 
 
+@Document(indexName = "cards")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -26,33 +30,51 @@ public class Card {
     @GeneratedValue(generator = "UUID")
     public UUID id;
 
+    @Field(type = FieldType.Text)
     public String stage;
 
+    @Field(type = FieldType.Text)
     @NotBlank(message = "Name is mandatory")
     public String name;
 
+    @Field(type = FieldType.Short)
     @Column(columnDefinition = "smallint")
     public short hp;
 
+    @Field(type = FieldType.Object)
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "evolves_from_id")
     public Card evolvesFrom;
 
+    @Field(type = FieldType.Nested)
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "JSON")
     @JsonDeserialize(using = SkillDeserializer.class)
     public List<AttackSkill> attackSkills;
 
+    @Field(type = FieldType.Text)
     public String weaknessType;
+
+    @Field(type = FieldType.Text)
     public String resistanceType;
+
+    @Field(type = FieldType.Text)
     public String retreatCost;
+
+    @Field(type = FieldType.Text)
     public String gameSet;
+
+    @Field(type = FieldType.Text)
     public String pokemonType;
+
+    @Field(type = FieldType.Keyword)
     public char regulationMark;
 
+    @Field(type = FieldType.Object)
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "pokemon_owner_id")
     public Student pokemonOwner;
 
+    @Field(type = FieldType.Text)
     public String cardNumber;
 }
